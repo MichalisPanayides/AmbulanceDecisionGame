c875a24012716a2f76763e9a19cf20f0af483d1f
Michalis Panayides <PanayidesM@cardiff.ac.uk>
2020-06-12 15:02:18 +0100

    Tests for overall waiting time

--- tests/test_markov_model.py
+++ tests/test_markov_model.py
@@ -503,7 +503,6 @@ def test_get_mean_number_of_patients():
 
 
 def test_get_mean_waiting_time_recursively_markov():
-    # TODO: add test for overall waiting time
     mean_waiting_time = get_mean_waiting_time_markov(
         lambda_a=0.2,
         lambda_o=0.2,
@@ -542,10 +541,22 @@ def test_get_mean_waiting_time_recursively_markov():
         formula="recursive",
     )
     assert mean_waiting_time == 0
+
+    mean_waiting_time = get_mean_waiting_time_markov(
+        lambda_a=0.2,
+        lambda_o=0.2,
+        mu=0.2,
+        num_of_servers=3,
+        threshold=4,
+        system_capacity=10,
+        parking_capacity=10,
+        output="both",
+        formula="recursive",
+    )
+    assert round(mean_waiting_time, number_of_digits_to_round) == round(1.1051493390764142, number_of_digits_to_round)
 
 
 def test_get_mean_waiting_time_from_closed_form_markov():
-    # TODO: add test for overall waiting time
     mean_waiting_time = get_mean_waiting_time_markov(
         lambda_a=0.2,
         lambda_o=0.2,
@@ -584,3 +595,16 @@ def test_get_mean_waiting_time_from_closed_form_markov():
         formula="closed_form",
     )
     assert mean_waiting_time == 0
+
+    mean_waiting_time = get_mean_waiting_time_markov(
+        lambda_a=0.2,
+        lambda_o=0.2,
+        mu=0.2,
+        num_of_servers=3,
+        threshold=4,
+        system_capacity=10,
+        parking_capacity=10,
+        output="both",
+        formula="closed_form",
+    )
+    assert round(mean_waiting_time, number_of_digits_to_round) == round(1.1051493390764142, number_of_digits_to_round)

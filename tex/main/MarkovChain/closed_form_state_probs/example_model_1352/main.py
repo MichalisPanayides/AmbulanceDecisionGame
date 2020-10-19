31783de1903ea24e4422d91a27c68e85d8b928de
Michalis Panayides <PanayidesM@cardiff.ac.uk>
2020-10-19 16:14:47 +0300

    Update files that generate tikz images
    Update the module from which these files are called from. On the
    previous branch the folder additional/ was renamed to tikz/ and
    these files were not updated. This commit updates these files.

--- tex/main/MarkovChain/closed_form_state_probs/example_model_1352/main.py
+++ tex/main/MarkovChain/closed_form_state_probs/example_model_1352/main.py
@@ -1,6 +1,6 @@
 import ambulance_game as abg
 
-latex_code = abg.markov.additional.generate_code_for_tikz_figure(1, 3, 5, 2)
+latex_code = abg.markov.tikz.generate_code_for_tikz_figure(1, 3, 5, 2)
 
 
 with open("main.tex", "w") as textfile:

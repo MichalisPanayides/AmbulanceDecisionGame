ae1e8286c09abaf7c1e45e3a51dc91ea8a4a5e49
Michalis Panayides <PanayidesM@cardiff.ac.uk>
2021-12-20 12:55:25 +0000

    Release version 0.0.3 (#60)
    * Update changelog.md
    
    * Update __version__

--- src/ambulance_game/__init__.py
+++ src/ambulance_game/__init__.py
@@ -3,7 +3,7 @@ A library to study the gaming that takes place at the interface of hospitals
 and ambulances.
 """
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 
 import ambulance_game.comparisons
 import ambulance_game.game
